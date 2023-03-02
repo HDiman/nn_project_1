c38011fa772845c5eeefeb3e81df26fff5965b39
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-03-02 19:53:49 +0300

    Step 2

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Testing")
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print(self.name + " is barking!")
+
+dog1 = Dog("Rex", 3)
+dog1.bark()
+
+
